4926b98d279418afc062c64e7a83bbeab35a730f
Andean <andean.arippa@amarbank.co.id>
2025-06-25 10:00:54 +0700

    refactor: middleware and add mocks usecase book

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,14 +1,14 @@
 package middleware
 
 import (
+	"book-lending-api/pkg/response"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
 )
 
-func JWTMiddleware(secret string) gin.HandlerFunc {
+func JWTMiddleware(jwtService IJWTService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
@@ -18,18 +18,15 @@ func JWTMiddleware(secret string) gin.HandlerFunc {
 
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
 
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return []byte(secret), nil
-		})
-
-		if err != nil || !token.Valid {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+		claims, err := jwtService.VerifyToken(tokenString)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response.APIResponse{
+				Success: false, Error: "invalid or expired token",
+			})
 			return
 		}
 
-		claims := token.Claims.(jwt.MapClaims)
-		c.Set("user_id", claims["user_id"])
-		c.Set("email", claims["email"])
+		c.Set("userID", claims.UserID)
 
 		c.Next()
 	}
--- internal/middleware/jwt.go
+++ internal/middleware/jwt.go
@@ -2,12 +2,20 @@ package middleware
 
 import (
 	"book-lending-api/config"
+	"errors"
 	"github.com/golang-jwt/jwt/v5"
 	"time"
 )
 
+// mockery --name=IJWTService         --dir=internal/middleware         --output=internal/middleware/mocks         --with-expec
 type IJWTService interface {
 	GenerateToken(userID int64, email string) (string, error)
+	VerifyToken(token string) (*CustomClaims, error)
+}
+
+type CustomClaims struct {
+	UserID int64 `json:"user_id"`
+	jwt.RegisteredClaims
 }
 
 type JwtService struct {
@@ -30,4 +38,19 @@ func (s *JwtService) GenerateToken(userID int64, email string) (string, error) {
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(s.secretKey))
+}
+
+func (s *JwtService) VerifyToken(tokenStr string) (*CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &CustomClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(s.secretKey), nil
+	})
+	if err != nil || !token.Valid {
+		return nil, errors.New("invalid or expired token")
+	}
+
+	claims, ok := token.Claims.(*CustomClaims)
+	if !ok {
+		return nil, errors.New("could not parse claims")
+	}
+	return claims, nil
 }
